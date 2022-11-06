0bf0ea7debac3dacb93b0d2b976587671ee1b218
Miompolly <57joel39@gmail.com>
2022-11-06 15:45:52 +0200

    Usestate

--- src/components/HookCountTree.js
+++ src/components/HookCountTree.js
@@ -5,11 +5,11 @@ function HookCountTree() {
   return (
     <form>
         <br></br>
-<input type='text' value={name.firstName} onChange={e=>setName({firstName:e.target.value})}/><br/><br/>
-<input type='text' value={name.lastName} onChange={e=>setName({lastName:e.target.value})}/>
+<input type='text' value={name.firstName} onChange={e=>setName({...name,firstName:e.target.value})}/><br/><br/>
+<input type='text' value={name.lastName} onChange={e=>setName({...name,lastName:e.target.value})}/>
 <h1>Your FistName :{name.firstName}</h1>
 <h1>Your LastName :{name.lastName}</h1>
-
+<h2>{JSON.stringify(name)}</h2>
     </form>
   )
 }
