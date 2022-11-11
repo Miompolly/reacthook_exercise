378403257e3b33783d6f3cf6d2e937498833dac3
Miompolly <57joel39@gmail.com>
2022-11-11 12:09:38 +0200

    conditionally run effect

--- src/components/HookCountFour.jsx
+++ src/components/HookCountFour.jsx
@@ -2,14 +2,18 @@ import React,{useState,useEffect} from 'react'
 
 function HookCountFour() {
     const [count,setCount]=useState(0)
+    const [name,setName]=useState('')
     useEffect(()=>{
-document.title=`You clicked ${count} times `
-    })
+      console.log("updating document name")
+      document.title=`You clicked ${count} times `
+
+
+    },[count])
 
     
   return (
     <div>
-       
+       <input type="text" value={name} onChange={e => setName(e.target.value)} />
         <button onClick={()=>setCount(prev=>prev+1)}>You clicked {count} times</button>
 
     </div>
