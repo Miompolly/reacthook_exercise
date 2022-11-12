9496b47e0b731edcc4d79921cff742bb0d0ea6cd
Miompolly <57joel39@gmail.com>
2022-11-12 15:57:09 +0200

    Data fetching axios 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import './App.css';
-import HookCount from './components/HookCount';
-import HookCounterTwo from './components/HookCounterTwo';
-import HookCountFive from './components/HookCountFive';
-import HookCountFour from './components/HookCountFour';
-import HookCountTree from './components/HookCountTree';
+// import HookCount from './components/HookCount';
+// import HookCounterTwo from './components/HookCounterTwo';
+// import HookCountFive from './components/HookCountFive';
+// import HookCountFour from './components/HookCountFour';
+// import HookCountTree from './components/HookCountTree';
+import DataFechting from './components/DataFechting';
 
 function App() {
   return (
     <div className="App">
-   <HookCount />
+   {/* <HookCount />
   < HookCounterTwo/>
  <HookCountTree/>
  <HookCountFour/>
- <HookCountFive/>
+ <HookCountFive/> */}
+<DataFechting/>
     </div>
   );
 }
